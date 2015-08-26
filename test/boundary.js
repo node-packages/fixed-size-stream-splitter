843a597d64ee60bcdf5df571c7c9b7b8cc2f2f6a
James Halliday <mail@substack.net>
2015-08-26 14:40:11 -0700

    failing boundary test

---
+++ test/boundary.js
@@ -0,0 +1,24 @@
+var test = require('tape')
+var splitter = require('../')
+var concat = require('concat-stream')
+var randomBytes = require('crypto').randomBytes
+var chunky = require('chunky')
+
+test('boundary', function (t) {
+  t.plan(3)
+  var data = randomBytes(300)
+  var chunks = chunky(data)
+  var index = 0
+  var w = splitter(100, function (stream) {
+    var i = index
+    index += 100
+    stream.pipe(concat(function (body) {
+      t.deepEqual(body, data.slice(i, i + 100))
+    }))
+  })
+  ;(function next () {
+    if (chunks.length === 0) return w.end()
+    w.write(chunks.shift())
+    process.nextTick(next)
+  })()
+})
